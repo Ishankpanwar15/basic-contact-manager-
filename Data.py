fdf734a34415ede1c13ad74c3a4c99f5ab6bd4f8
Ishank Panwar <155763704+Ishankpanwar15@users.noreply.github.com>
2025-03-31 15:21:39 +0530

    Update Data.py
    delete the contact

--- Data.py
+++ Data.py
@@ -9,6 +9,24 @@ def add_person():
 def display_people(people):
     for i, person in enumerate(people):
         print(i + 1, "-", person["name"], "|", person["age"], "|", person["email"])
+
+def delete_contact(people):
+    display_people(people)
+
+    while True:
+        number = input("Enter a number to delete: ")
+        try: 
+            number = int(number)
+            if number <= 0 or number > len(people):
+                print("Invalid number,  out of range. ")
+            else:
+                break
+        except:
+            print("Invalid number")
+
+
+    people.pop(number - 1)
+    print("person deleted")
         
 
 print("Hii, welcome to the Contact Management System.")
@@ -23,6 +41,8 @@ while True:
         people.append(person)
         display_people(people)
         print("person added")
+    elif command == "delete":
+        delete_contact(people)
     else:
         print("invalid input")
  
